2b3e369dd474dcb9e653ac16977969d6ae62fbe9
alentmathew <alendevops@gmail.com>
2021-10-10 09:16:37 +0000

    eks ckuster terraform code template

---
+++ kube.tf
@@ -0,0 +1,11 @@
+resource "null_resource" "kube_config_import" {
+  provisioner "local-exec" {
+    command = "aws eks --region us-east-1 update-kubeconfig --name ${aws_eks_cluster.cluster1.name}"
+  }
+
+  depends_on = [
+      aws_eks_cluster.cluster1,
+      aws_eks_node_group.demo
+  ]
+}
+
---
+++ terraform.tfvars
@@ -0,0 +1,3 @@
+clustername = "demo-eks-cluster"
+subnet = [ "subnet-07bbc245f4813ef82", "subnet-0f13bdf4557b88231" ]
+
